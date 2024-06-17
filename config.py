614b159a5bbf72e6977f367920f9e9b42ff5ac02
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-17 23:30:07 +0300

    Update config.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", "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"
+    "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alemciyiz")
