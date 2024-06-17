d28f59f6d6ad8f3e500de1a2841e49e1e6b471fa
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-17 21:52:42 +0300

    Update config.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,11 +32,11 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/AdanaliMuhendis/alem",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alemciyiz")
