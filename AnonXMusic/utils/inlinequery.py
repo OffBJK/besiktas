13694c897bde3a86ca855ef5d30fb0fa6d1c0373
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-18 17:50:43 +0300

    Update inlinequery.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- AnonXMusic/utils/inlinequery.py
+++ AnonXMusic/utils/inlinequery.py
@@ -5,37 +5,37 @@ answer = []
 answer.extend(
     [
         InlineQueryResultArticle(
-            title="Pᴀᴜsᴇ",
+            title="Pᴀᴜsᴇ, dur",
             description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
             thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
             input_message_content=InputTextMessageContent("/pause, /dur"),
         ),
         InlineQueryResultArticle(
-            title="Rᴇsᴜᴍᴇ",
+            title="Rᴇsᴜᴍᴇ, devam",
             description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
             thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
             input_message_content=InputTextMessageContent("/resume, /devam"),
         ),
         InlineQueryResultArticle(
-            title="Sᴋɪᴩ",
+            title="Sᴋɪᴩ, atla",
             description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
             thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
             input_message_content=InputTextMessageContent("/skip, /atla"),
         ),
         InlineQueryResultArticle(
-            title="Eɴᴅ",
+            title="Eɴᴅ, son",
             description="ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
             thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
             input_message_content=InputTextMessageContent("/end, /son"),
         ),
         InlineQueryResultArticle(
-            title="Sʜᴜғғʟᴇ",
+            title="Sʜᴜғғʟᴇ, karıştır",
             description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
             thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
             input_message_content=InputTextMessageContent("/shuffle, /karıştır"),
         ),
         InlineQueryResultArticle(
-            title="Lᴏᴏᴩ",
+            title="Lᴏᴏᴩ, ileri, geri",
             description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
             thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
             input_message_content=InputTextMessageContent("/loop 3, /ileri, /geri"),
