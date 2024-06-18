411d52ef884068e99aa7ace9c0c002b1cbddda07
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-18 17:42:17 +0300

    Update inlinequery.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- AnonXMusic/utils/inlinequery.py
+++ AnonXMusic/utils/inlinequery.py
@@ -8,7 +8,7 @@ answer.extend(
             title="Pᴀᴜsᴇ",
             description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
             thumb_url="https://telegra.ph/file/c5952790fa8235f499749.jpg",
-            input_message_content=InputTextMessageContent("/pause, /oynat"),
+            input_message_content=InputTextMessageContent("/pause, /dur"),
         ),
         InlineQueryResultArticle(
             title="Rᴇsᴜᴍᴇ",
