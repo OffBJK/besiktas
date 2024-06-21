448ab946284a9e3adfb5baa4b67cd33cf0ff809c
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-21 16:14:00 +0300

    Update config.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alemciyiz")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SohbetAlemi")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", false))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
@@ -54,7 +54,7 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "709e1a2969664491b582008
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 50))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 15))
 
 
 # Telegram audio and video file size limit (in bytes)
