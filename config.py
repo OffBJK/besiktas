591fdb17522b18a53db8b797ac75bd64784d0bb8
OffBJK <off.bjk.01@gmail.com>
2024-06-22 20:07:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/OffBJK/besiktas",
+    "https://github.com/AdanaliMuhendis/besiktas",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
